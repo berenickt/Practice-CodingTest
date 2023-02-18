fed2afc3d9469457e79d40242a6254b0150bcb96
Seungwoo Yoo <berenickt99@gmail.com>
2023-02-18 21:04:30 +0900

    중복순열-동전교환(DFS)

--- 02_JS-Basic/08_재귀함수와 완전탐색(DFS-깊이우선)/08_중복순열(다중 for문과 재귀 차이점).js
+++ 02_JS-Basic/08_재귀함수와 완전탐색(DFS-깊이우선)/08_중복순열(다중 for문과 재귀 차이점).js
@@ -1,27 +1,34 @@
 /**
  * 1부터 N까지 번호가 적힌 구슬이 있습니다.
  * 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열하는 방법을 모두 출력
- * @param {*} n 자연수 N(3<=N<=10)
- * @param {*} m 자연수 M(2<=M<=N)
- * @returns
+ *
+ * @param {*} bead 번호가 적힌 구슬 ( 3 <= N <= 10 )
+ * @param {*} pick 뽑는 횟수, 중복 가능
+ * @returns 구슬을 여러 번 뽑아서 일렬로 나열하는 경우의 수
  */
-
-function solution(n, m) {
+function solution(bead, pick) {
   let answer = [];
-  let tmp = Array.from({ length: m }, () => 0);
-  function DFS(L) {
-    if (L === m) {
-      answer.push(tmp.slice());
-    } else {
-      for (let i = 1; i <= n; i++) {
-        tmp[L] = i;
-        DFS(L + 1);
+  let temp = Array.from({ length: pick }, () => 0); // 뽑는 횟수가 저장되는 곳
+
+  function DFS(level) {
+    // 뽑은 횟수만큼 되면, 얕은 복사로 새 배열 객체를 정답배열에 넣기
+    if (level === pick) answer.push(temp.slice());
+    else {
+      // 번호가 적힌 구슬까지 순회
+      for (let index = 1; index <= bead; index++) {
+        temp[level] = index;
+        DFS(level + 1);
       }
     }
   }
-
   DFS(0);
   return answer;
 }
 
 console.log(solution(3, 2));
+// 3*3 = 9가지의 경우의 수
+// [
+//   [ 1, 1 ], [ 1, 2 ], [ 1, 3 ],
+//   [ 2, 1 ], [ 2, 2 ], [ 2, 3 ],
+//   [ 3, 1 ], [ 3, 2 ], [ 3, 3 ]
+// ]
--- 02_JS-Basic/08_재귀함수와 완전탐색(DFS-깊이우선)/09_동전교환(DFS-Cut Edge Tech).js
+++ 02_JS-Basic/08_재귀함수와 완전탐색(DFS-깊이우선)/09_동전교환(DFS-Cut Edge Tech).js
@@ -3,26 +3,30 @@
  * 거스름돈을 가장 적은 수의 동전으로 교환해주려면 어떻게 주면 되는가?
  * 각 단위의 동전은 무한정 쓸 수 있음
  *
- * cf. 첫 번째 줄에는 동전의 종류개수 N(1<=N<=12)이 주어진다.
- * 두 번째 줄에는 N개의 동전의 종류가 주어지고,
- * 그 다음줄에 거슬러 줄 금액 M(1<=M<=500)이 주어진다.
- * 각 동전의 종류는 100원을 넘지 않는
- * @param {*} m
- * @param {*} arr
- * @returns
+ * @param {*} changeMoney 거스름돈 (1 <= M<= 500)
+ * @param {*} coinType 동전 종류가 들어있는 배열 (1 <= N <= 12)
+ * 각 동전의 종류는 100원을 넘지 않음
+ * @returns 거슬러줄 동전의 최소 개수
  */
-
-function solution(m, arr) {
+function solution(changeMoney, coinType) {
   let answer = Number.MAX_SAFE_INTEGER;
-  let n = arr.length;
-  function DFS(L, sum) {
-    if (sum > m) return;
-    if (L >= answer) return;
-    if (sum === m) {
-      answer = Math.min(answer, L);
-    } else {
-      for (let i = 0; i < n; i++) {
-        DFS(L + 1, sum + arr[i]);
+
+  function DFS(level, sum) {
+    // 동전누적합이 거스름돈을 넘으면, 종료
+    if (sum > changeMoney) return;
+    // ** 15원을 만들기 위해 다른 가지로 올라가서 더 안만들게, 효율성을 위해
+    // 동전개수가 기존답(가지)보다 크면, 종료
+    if (level >= answer) return;
+
+    // 동전누적합이 거스름돈이 되면, 최소한의 동전개수를 정답으로 넣기
+    if (sum === changeMoney) {
+      console.log(level, sum);
+      answer = Math.min(answer, level);
+    }
+    // 동전종류만큼 순회
+    else {
+      for (let index = 0; index < coinType.length; index++) {
+        DFS(level + 1, sum + coinType[index]);
       }
     }
   }
@@ -30,5 +34,5 @@ function solution(m, arr) {
   return answer;
 }
 
-let arr = [1, 2, 5];
-console.log(solution(15, arr));
+console.log(solution(15, [1, 2, 5])); // 3
+// 5원짜리 동전 3개로 15원을 거슬러줄 수 있음
