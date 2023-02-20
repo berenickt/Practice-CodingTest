49b99c1bc25afc7141e012753c27c5e63b00ec12
Seungwoo Yoo <berenickt99@gmail.com>
2023-02-20 18:44:23 +0900

    DFS-조합의경우수-수열추측하기

--- 03_JS-Programmers/고득점-kit/01_해시/1_level-1-폰켓몬.js
+++ 03_JS-Programmers/고득점-kit/01_해시/1_level-1-폰켓몬.js
@@ -1,7 +1,7 @@
 /** 폰켓몬, https://school.programmers.co.kr/learn/courses/30/lessons/1845
  * @param {Array} nums 각각의 폰켓몬
- * @returns
- * 폰켓몬 N/2마리 중 선택할 수 있는 폰켓몬 종류의 최대값
+ * @returns 폰켓몬 N/2마리 중 선택할 수 있는 폰켓몬 종류의 최대값
+ *
  * 1. nums배열 안에 중복되는 폰켓몬들을 제거한 후
  * 2. [중복 제거한 폰켓몬 수] >  [N/2]라면 답은 [N/2]
  * 3. [중복 제거한 폰켓몬 수] <  [N/2]라면 답은 [중복 제거한 폰켓몬 수]
