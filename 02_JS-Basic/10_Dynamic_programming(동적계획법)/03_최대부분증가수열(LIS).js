cd7a8acc82d5e7e860d485759bf194a47f2f83a7
Beren <berenickt99@gmail.com>
2023-03-03 21:17:20 +0900

    dynamic-돌다리건너기-최대부분증가수열

--- 02_JS-Basic/10_Dynamic_programming(동적계획법)/03_최대부분증가수열(LIS).js
+++ 02_JS-Basic/10_Dynamic_programming(동적계획법)/03_최대부분증가수열(LIS).js
@@ -6,27 +6,33 @@
  * 원소들을 차례대로 뽑아내면 2, 5, 6, 7, 12를 뽑아내어
  * 길이가 5인 최대 부분 증가수열을 만들 수 있다
  *
- * 첫째 줄은 입력되는 데이터의 수 N(1≤N≤1,000, 자연수)를 의미하고,
- * 둘째 줄은 N개의 입력데이터들이 주어짐
- * @param {*} arr
+ * @param {*} arr n개의 자연수 수열 ( 1 <= n <= 1000 )
  * @returns 부분증가수열의 최대 길이
  */
 function solution(arr) {
   let answer = 0;
   let dy = Array.from({ length: arr.length }, () => 0);
-  dy[0] = 1;
+  dy[0] = 1; // 가장 처음 있는 값은 자기자신의 길이만 가짐
+
+  // arr 순회
   for (let i = 1; i < arr.length; i++) {
     let max = 0;
+
+    // 뒤에서부터 앞으로 순회
     for (let j = i - 1; j >= 0; j--) {
+      // 뒤에 항이 앞에 항보다 작거나, 뒤에 항이 최대값보다 크면
       if (arr[j] < arr[i] && dy[j] > max) {
+        // 그 뒤에 항을 최대값으로
         max = dy[j];
       }
     }
+
+    // if문 만족하는게 하나도 없다면, 자기자신만 더하기
+    // 최대값이 있으면, 만족한 값의 경우에다, 자기자신 더하기
     dy[i] = max + 1;
     answer = Math.max(answer, dy[i]);
   }
   return answer;
 }
 
-let arr = [5, 3, 7, 8, 6, 2, 9, 4];
-console.log(solution(arr));
+console.log(solution([5, 3, 7, 8, 6, 2, 9, 4])); // 4
