31afc5f8fc632a002002c4fbe2195918d6eef160
Beren <berenickt99@gmail.com>
2023-03-14 22:37:45 +0900

    고득점-Hash-위장

--- 06_Programmers-HighScore-kit/01_해시/4_level-2_위장.js
+++ 06_Programmers-HighScore-kit/01_해시/4_level-2_위장.js
@@ -4,23 +4,30 @@
  * @returns 서로 다른 옷의 조합의 수
  * cf. clothes의 각 행은 [의상의 이름, 의상의 종류]로 이루어짐
  *
- * 각 종류마다 의상의 수에 안입는 경우(1)를 더해 곱해주고, 모두 입지 않는 경우(1)를 빼서 return
+ * 풀이
+ * 1. 종류별로 분류 - 옷을 종류별로 구분
+ * 2. 입지 않는 경우 추가 - 종류별로 하나씩 입지 않아도 되니, 입지 않아도 되는 경우를 추가
+ * 3. 전체 조합 - 전체 조합을 계산하고, 아무것도 입지 않는 경우의 수를 빼준다.
+ *
+ * 종류별로 경우의 수를 구할 떄는 Hash를 사용하는 것이 편리합니다.
  */
 function solution(clothes) {
   let answer = 1;
   const hash = new Map();
 
-  for (let style of clothes) {
-    // [의상의종류]가 있다면, 추가(의상의종류, 찾기[의상의종류] + 1)
-    if (hash.has(style[1])) hash.set(style[1], hash.get(style[1]) + 1);
-    // 없다면, 추가(의상의종류, 1)
-    else hash.set(style[1], 1);
+  // **** 1. 종류별로 분류
+  for (let [value, key] of clothes) {
+    // 추가(키, 키가 있으면 기존 키 + 1 || 키가 없으면 1로 초기화)
+    hash.set(key, hash.get(key) + 1 || 1);
   }
 
-  // [의상의종류키, 나온횟수]
-  for (let [key, value] of hash) answer *= value + 1;
+  // **** 2. 입지 않는 경우 추가
+  for (let [key, value] of hash) {
+    // console.log(`${key}는 ${value}개 있다!`);
+    answer *= value + 1;
+  }
 
-  // 최소 1개 이상은 입으므로 아예 입지 않는 경우 1을 빼준다
+  // **** 3. 최소 1개 이상은 입으므로 아예 입지 않는 경우 1을 빼준다
   return answer - 1;
 }
 
@@ -30,5 +37,14 @@ console.log(
     ['yellow_hat', 'headgear'],
     ['blue_sunglasses', 'eyewear'],
     ['green_turban', 'headgear'],
-  ])
+  ]),
 ); // 5
+
+/*** Map 객체
+ * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Map
+ * has() : 존재하는지 확인
+ * get() : 찾기
+ * set() : 추가
+ * delete() : 삭제
+ * clear() : 전부삭제
+ */
